41126c2798f093ade700dfed17da17df48b5e321
Dima2065 <samarskydmitry@gmail.com>
2026-05-06 09:21:44 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "family-tasks-v56";
+const CACHE_NAME = "family-tasks-v57";
 const ASSETS = [
   "./",
   "index.html",
@@ -52,4 +52,3 @@ self.addEventListener("fetch", function(event) {
     })
   );
 });
-
